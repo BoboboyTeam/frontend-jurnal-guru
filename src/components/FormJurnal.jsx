9391a52a06b8541f7a3a52a37cc1c561fc7a3718
AL1Skey <adamnurramadan@gmail.com>
2024-07-20 16:22:30 +0700

    [FIX] Wiring Form Jurnal

---
+++ src/components/FormJurnal.jsx
@@ -0,0 +1,302 @@
+import axios from "axios";
+import React, { useCallback, useEffect, useState } from "react";
+import { Icon } from "react-icons-kit";
+import { user } from "react-icons-kit/icomoon/user";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+
+
+
+const FormJurnal = () => {
+  function handleLogout() {
+
+  }
+
+  const day = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+  const [guru,setGuru] = useState([])
+  const kelas = ["VII", "VIII", "IX"];
+  const jam = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  const mataPelajaran = [
+    "Matematika",
+    "Bahasa Inggris",
+    "Bahasa Jawa",
+    "IPA",
+    "IPS",
+    "Biologi",
+  ];
+
+  const navigate = useNavigate();
+
+  async function handleLogout() {
+    localStorage.removeItem("access_token");
+    navigate("/login");
+  }
+
+  // ------------------------------------------------------Get all data
+
+  
+  const fetchData = useCallback(async()=>{
+    try {
+      const token = localStorage.getItem("access_token");
+      const { data } = await axios({
+        method: "get",
+        url: "http://localhost:3000/users/guru",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setGuru(data);
+      console.log(JSON.stringify(guru));
+      guru.map((item) => {
+        console.log(item.nama);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },[guru])
+
+  const postJurnalGuru = useCallback(async (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem("access_token");
+    const form = new FormData(e.target);
+
+    // Dapetin guru dan _idnya
+    let guru_id = form.get("guru");
+    let guruPengganti_id = form.get("guruPengganti");
+    guru_id = guru.find((item) => item._id === guru_id);
+    guruPengganti_id = guru.find((item) => item._id === guruPengganti_id);
+
+    console.log(guru_id);
+
+    const formData = {
+      hari: form.get("hari"),
+      jamKe: form.get("jamKe"),
+      guru: {
+        _id: guru_id._id,
+        nama: guru_id.nama,
+      },
+      guruPengganti:
+      guruPengganti && {
+        _id: guruPengganti_id._id,
+        nama: guruPengganti_id.nama,
+      },
+      kelas: form.get("kelas"),
+      mapel: form.get("mapel"),
+      materi: form.get("materi"),
+      jp: form.get("jp"),
+    };
+    console.log(formData);
+    
+    try {
+      const { data } = await axios({
+        method: "post",
+        url: "http://localhost:3000/admin/jurnal-guru",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        data: JSON.stringify(formData),
+      });
+      console.log(data);
+      
+      Swal.fire({
+        icon: "success",
+        title: "Succes Adding Jurnal",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+
+
+
+
+  useEffect(()=>{
+    fetchData()
+  },[])
+
+
+
+  return (
+    <>
+      <div
+        style={{
+          backgroundImage:
+            'url("https://images.unsplash.com/photo-1541829070764-84a7d30dd3f3?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+        }}
+        className="items-center justify-center p-12  "
+      >
+        <div className="flex justify-end mb-3  ">
+          <div className=" bg-yellow-600 inline-block px-2 py-2 rounded-md hover:bg-yellow-700">
+         <button onClick={handleLogout}>   <Icon size={30} icon={user}></Icon> Logout{" "}</button>
+          </div>
+        </div>
+        <div className="mx-auto w-full max-w-[600px] p-10 bg-black bg-opacity-50 rounded-md shadow-lg  ">
+          <form onSubmit={postJurnalGuru}>
+            <div className="md:flex md:gap-28">
+              <div>
+                <label
+                  htmlFor="hari"
+                  className="mb-3 block text-base font-medium text-white"
+                >
+                  Hari
+                </label>
+
+                <div className="mb-5 bg-white p-3 rounded-md">
+                  <select className="w-full" id="hari" name="hari">
+                    {day.map((item,index) => {
+                      return (
+                        <>
+                          <option key={index} value="hari">{item}</option>
+                        </>
+                      );
+                    })}
+                  </select>
+                </div>
+
+                <div className="mb-5">
+                  <label
+                    htmlFor="jamKe"
+                    className="mb-3 block text-base font-medium text-white"
+                  >
+                    Jam ke
+                  </label>
+                  <div className="mb-5 bg-white p-3 rounded-md">
+                    <select className="w-full" id="jamKe" name="jamKe">
+                      {jam.map((item,index) => {
+                        return (
+                          <>
+                            <option key={index} value={item}>{item}</option>
+                          </>
+                        );
+                      })}
+                    </select>
+                  </div>
+                </div>
+
+                <label
+                  htmlFor="guru"
+                  className="mb-3 block text-base font-medium text-white"
+                >
+                  Guru
+                </label>
+
+                <div className="mb-5 bg-white p-3 rounded-md">
+                  <select className="w-full" id="guru" name="guru">
+                    <option value="">None</option>
+                    {guru.map((item) => {
+                        return (
+                        <option key={item._id} value={item._id}>{item.nama}</option>
+                        );
+                    })}
+                  </select>
+                </div>
+
+                <label
+                  htmlFor="guruPengganti"
+                  className="mb-3 block text-base font-medium text-white"
+                >
+                  Guru Pengganti{" "}
+                  <span className="font-light text-sm">(Opsional)</span>
+                </label>
+
+                <div className="mb-5 bg-white p-3 rounded-md">
+                  <select className="w-full" id="guruPengganti" name="guruPengganti">
+                  <option value="">None</option>
+                    {guru?.map((item) => {
+                      return (
+                        <>
+                          <option key={item._id} value={item._id}>{item.nama}</option>
+                        </>
+                      );
+                    })}
+                  </select>
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="kelas"
+                  className="mb-3 block text-base font-medium text-white"
+                >
+                  Kelas
+                </label>
+
+                <div className="mb-5 bg-white p-3 rounded-md">
+                  <select className="w-full" id="kelas" name="kelas">
+                    {kelas.map((item,index) => {
+                      return (
+                        <>
+                          <option key={index} value={item}>{item}</option>
+                        </>
+                      );
+                    })}
+                  </select>
+                </div>
+
+                <div className="mb-5">
+                  <label
+                    htmlFor="mapel"
+                    className="mb-3 block text-base font-medium text-white"
+                  >
+                    Mata Pelajaran
+                  </label>
+                  <div className="mb-5 bg-white p-3 rounded-md">
+                    <select className="w-full" id="mapel" name="mapel">
+                      {mataPelajaran.map((item,index) => {
+                        return (
+                          <>
+                            <option key={index} value={item}>{item}</option>
+                          </>
+                        );
+                      })}
+                    </select>
+                  </div>
+                </div>
+
+                <div className="mb-5">
+                  <label
+                    htmlFor="subject"
+                    className="mb-3 block text-base font-medium text-white"
+                  >
+                    Materi Pembelajaran
+                  </label>
+                  <input
+                    type="text"
+                    name="materi"
+                    id="subject"
+                    placeholder="Enter your subject"
+                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  />
+                </div>
+
+                <div className="mb-5">
+                  <label
+                    htmlFor="subject"
+                    className="mb-3 block text-base font-medium text-white"
+                  >
+                    Jumlah Jam Pelajaran
+                  </label>
+                  <input
+                    type="text"
+                    name="jp"
+                    id="subject"
+                    placeholder="Enter your subject"
+                    className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <button type="submit" className="text-white rounded-md border-none hover:bg-yellow-600 bg-yellow-400 bg-opacity-50 px-10 py-3 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md" >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FormJurnal;
